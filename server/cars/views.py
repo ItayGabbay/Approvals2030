a1a6af2160dd5a00033878b38e90f92fc854bc09
Or Moshe <ormoshe2204@gmail.com>
2019-07-29 18:31:49 +0300

    push pashut

--- server/cars/views.py
+++ server/cars/views.py
@@ -4,10 +4,16 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from .utils import *
 from .models import Approvals
+from cv2 import imread
 
-import numpy as np
 from .models import Approvals
+import requests
 
+MAIN_SERVER_HOST = '1.2.3.4'
+
+@csrf_exempt
+def index(request):
+    return 200
 
 @csrf_exempt
 @require_POST
@@ -24,9 +30,33 @@ def validate_person(request):
     license_number = body.POST.get('license_number')
 
     try:
-        person = Approvals.objects.filter(license_number=license_number, is_authorized=True).values('picture')
+        persons = Approvals.objects.filter(is_authorized=True)
     except:
         return HttpResponse(False)    
 
+    all_faces = [imread(p.picture) for p in persons]
 
-    return HttpResponse(True)
+    data= {
+        'face': face,
+        'all_faces': all_faces
+    }
+    res = requests.post(MAIN_SERVER_HOST, data=data)
+    if res.status_code != 200:
+        return HttpResponse(False)
+    predictions = jsonpickle.loads(res.content)
+    
+
+    if sum_preds sum(predictions) = 1:
+        return HttpResponse(404)
+    
+    # Else - sum == 1:
+
+    try:
+        index = predictions.index(True)
+    except AttributeError:
+        return HttpResponse(False)
+    
+    if persons[index].license_number == license:
+        return HttpResponse(True)
+
+    return HttpResponse(False)
