cfefc968378cd66262102a6fa959457800bd8528
Or Moshe <ormoshe2204@gmail.com>
2019-07-29 14:03:46 +0300

    modify model

--- server/cars/models.py
+++ server/cars/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Approvals(models.Model):
-    person_id = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     license_number = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
+    picture = models.CharField()
